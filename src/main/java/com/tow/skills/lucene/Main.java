b741cac2d4d73b3af07ee23d483b4af05b0deb33
Sergey <drenozua@gmail.com>
2018-01-17 01:20:05 +0200

    Add search

--- src/main/java/com/tow/skills/lucene/Main.java
+++ src/main/java/com/tow/skills/lucene/Main.java
@@ -1,32 +1,61 @@
 package com.tow.skills.lucene;
 
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Analyzer analyzer = new WhitespaceAnalyzer();
+    public static void main(String[] args) throws IOException, ParseException {
+        Analyzer analyzer = new StandardAnalyzer();
 
         Directory directory = new RAMDirectory();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
 
         IndexWriter indexWriter = new IndexWriter(directory, config);
 
-
         Document doc = new Document();
-        String text = "Lucene is an Information Retrieval library written in Java.";
-        doc.add(new TextField("fieldName", text, Field.Store.YES));
+        String text = "Lucene is an Information Retrieval library written in Java. Lucene";
+        doc.add(new TextField("Content", text, Field.Store.YES));
 
         indexWriter.addDocument(doc);
         indexWriter.close();
+
+        IndexReader indexReader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+
+        QueryParser queryParser = new QueryParser("Content", analyzer);
+        Query query = queryParser.parse("Lucene");
+
+        int hitsPerPage =10;
+        TopDocs docs =indexSearcher.search(query,hitsPerPage);
+        ScoreDoc[] hits = docs.scoreDocs;
+        long end = Math.min(docs.totalHits, hitsPerPage);
+        System.out.println("Total Hits: "+docs.totalHits);
+        System.out.println("Results: ");
+
+        for (int i=0;i<end;i++){
+            Document d = indexSearcher.doc(hits[i].doc);
+            System.out.println("Content: "+d.get("Content"));
+        }
+
+//        System.out.println(indexSearcher.doc(0));
+//        System.out.println(indexSearcher.);
     }
+
 }
